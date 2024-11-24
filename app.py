44546b464713df622c61bab8a6c5274090a0c1f5
InspectorGoole <am6499057@gmail.com>
2024-11-25 00:04:18 +0400

    Flask application
    Created the flask application

---
+++ app.py
@@ -0,0 +1,29 @@
+import pickle
+
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd 
+
+
+app = Flask(__name__)
+
+## load the model
+modeltree = pickle.load(open('modeltree.pkl', 'rb')) 
+scalar = pickle.load(open('scaling.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST']) # this will not run we type /predict_api on the search bar bcuz the method is post, so we have to give it some information first
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1)) # scaling the data bcus the scaler expects a 2 dim array
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))  # the scaler gives out a one dim series and we have to turn it back to 2 dimensional
+    output = modeltree.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
